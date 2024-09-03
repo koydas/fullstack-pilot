dd0246f1f9bae7e2300323cb8ee1e7c35db59e5f
Stephane Hamel <koydas@hotmail.com>
2024-09-03 12:48:15 -0400

    dashboard project created

---
+++ dashboard/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a minimal React setup.</p>
+    </div>
+  );
+}
+
+export default App;
