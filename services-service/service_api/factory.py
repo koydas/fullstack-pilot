bf366ef1f40c3766b779f4d8807ba3072977ae84
Stephane Hamel <koydas@hotmail.com>
2025-12-13 17:45:30 -0500

    Add monitoring middleware to services-service

--- services-service/service_api/factory.py
+++ services-service/service_api/factory.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from flask import Flask
 
+from .monitoring import register_monitoring
 from .routes import services_bp
 
 
@@ -10,4 +11,5 @@ def create_app() -> Flask:
     app.config["SERVICES"] = {}
     app.config["NEXT_SERVICE_ID"] = 1
     app.register_blueprint(services_bp)
+    register_monitoring(app)
     return app
